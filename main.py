d847a4ad7299d128617c531674f24a07ad1dad18
genismoreno <gmr_800@hotmail.com>
2021-11-03 16:22:20 +0100

    parsing response in-memory

--- main.py
+++ main.py
@@ -4,24 +4,20 @@ with open('body.txt') as file:
 with open('headers.txt') as file:
     headers = file.read()
 
-import requests
-
-url = "https://sedeapl.dgt.gob.es:7443/WEB_NOTP_CONSULTA/consultaNota.faces"
-
 import email
 import io
 message = email.message_from_file(io.StringIO(headers))
 p_headers = dict(message.items())
 
+import requests
+
+url = "https://sedeapl.dgt.gob.es:7443/WEB_NOTP_CONSULTA/consultaNota.faces"
 resp = requests.post(url, headers=p_headers, data=body)
 content = resp.content
-with open('response.txt', 'wb') as file:
-    file.write(content)
 
 from lxml import html
-root = html.parse('response.txt').getroot()
-msg_errors = root.find_class("msgError")
-print(len(msg_errors))
+document = html.document_fromstring(content)
+msg_errors = document.find_class("msgError")
 for msg_error in msg_errors:
     print(html.tostring(msg_error))
 
